b44b4f35a064f9d4ac5175b5ba77a0a590e2eaaf
xianeng <xianeng@xin.com>
2018-01-19 10:43:53 +0800

    更新ReversetInteger算法

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,33 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by xianeng on 2018/1/18.
  */
 public class ReverseInteger {
 
     public static void main(String[] args) {
-        System.out.println(reverse(1534236469));
+        System.out.println(reverse(2147483647));
+        System.out.println(reverse(78591));
     }
 
 
     public static int reverse(int x) {
         int num = 0;
-        int temp = x;
 
-        List<Integer> list = new ArrayList<>();
-        while(temp != 0) {
-            list.add(temp % 10);
-            temp = temp / 10;
+        while(x != 0) {
+            int y = x % 10;
+            int newnum = num * 10 + (x % 10);
+            x = x / 10;
+            if((newnum - y) / 10 == num) {
+                num = newnum;
+            } else {
+                num = 0;
+                break;
+            }
         }
 
-        for(int i = 0;i<list.size();i++) {
-            num = (int) (num + list.get(i) * Math.pow(10, list.size() - i - 1));
-        }
 
-        if(num == Integer.MAX_VALUE || num == Integer.MIN_VALUE) {
-            num = 0;
-        }
 
         return num;
     }
